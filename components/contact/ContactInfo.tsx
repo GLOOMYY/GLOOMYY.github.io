f98d20e78f91602439dab62a9c342c47360b3492
Sebastian Mesa <sebas.mesa.montoya@gmail.com>
2025-02-18 00:33:46 -0500

    feat: links actualizados

--- components/contact/ContactInfo.tsx
+++ components/contact/ContactInfo.tsx
@@ -1,4 +1,5 @@
 import { Mail, MapPin, Phone } from "lucide-react"
+import { FaWhatsapp } from "react-icons/fa"
 
 const ContactInfo = () => (
   <div className="lg:w-1/2 bg-white/80 dark:bg-black/80 backdrop-blur-sm border border-gray-200 dark:border-blue-400 rounded-lg p-8 shadow-lg">
@@ -7,17 +8,33 @@ const ContactInfo = () => (
       <a
         href="mailto:sebas.mesa.montoya@gmail.com"
         className="flex items-center text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-200 transition-colors duration-300"
+        target="_blank"
+        rel="noopener noreferrer"
       >
         <Mail className="w-6 h-6 mr-3" />
         sebas.mesa.montoya@gmail.com
       </a>
+
+      <a
+        href="https://api.whatsapp.com/send/?phone=%2B573053438319&text&type=phone_number&app_absent=0"
+        className="flex items-center text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-200 transition-colors duration-300"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaWhatsapp className="w-6 h-6 mr-3" />
+        +57 305 343 8319
+      </a>
+
       <a
         href="tel:+573053438319"
         className="flex items-center text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-200 transition-colors duration-300"
+        target="_blank"
+        rel="noopener noreferrer"
       >
         <Phone className="w-6 h-6 mr-3" />
         +57 305 343 8319
       </a>
+
       <div className="flex items-center text-blue-600 dark:text-blue-400">
         <MapPin className="w-6 h-6 mr-3" />
         Medellin, Colombia
--- components/contact/SocialLinks.tsx
+++ components/contact/SocialLinks.tsx
@@ -2,13 +2,30 @@ import { User, Briefcase, Mail } from "lucide-react"
 
 const SocialLinks = () => (
   <div className="flex items-center justify-center space-x-6 mb-6">
-    <a href="#" className="text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-200 transition-colors">
+    <a 
+      href="https://github.com/GLOOMYY" 
+      target="_blank" 
+      rel="noopener noreferrer" 
+      className="text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-200 transition-colors"
+    >
       <User className="w-8 h-8" />
     </a>
-    <a href="#" className="text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-200 transition-colors">
+
+    <a 
+      href="https://api.whatsapp.com/send/?phone=%2B573053438319&text&type=phone_number&app_absent=0" 
+      target="_blank" 
+      rel="noopener noreferrer" 
+      className="text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-200 transition-colors"
+    >
       <Briefcase className="w-8 h-8" />
     </a>
-    <a href="#" className="text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-200 transition-colors">
+
+    <a 
+      href="mailto:sebas.mesa.montoya@gmail.com" 
+      target="_blank" 
+      rel="noopener noreferrer" 
+      className="text-blue-600 dark:text-blue-400 hover:text-blue-800 dark:hover:text-blue-200 transition-colors"
+    >
       <Mail className="w-8 h-8" />
     </a>
   </div>
